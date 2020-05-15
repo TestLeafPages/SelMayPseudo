76771fdd6ef40320563e4e8cec4fb20425595bdb
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-05-15 16:11:54 +0530

    Add files via upload

---
+++ week3/day2/classroom/VerifyTrainNamesAreUnique.java
@@ -0,0 +1,29 @@
+package week3.day2.classroom;
+
+public class VerifyTrainNamesAreUnique {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+//		Set the system property and Launch the URL
+		
+//		Click the 'sort on date' checkbox
+		
+//		clear and type in the from station text field
+		
+//		clear and type in the to station text field
+		
+//		Add a java sleep for 2 seconds
+		
+//		Store all the train names in a list
+		
+//		Get the size of list
+		
+//		Add the list into a new Set
+		
+//		Get the size of set
+
+//		Compare the Size of list and Set to verify the names are unique
+
+	}
+
+}
